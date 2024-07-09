99dc27be0bc31d1195030c91707814ea002faa99
Molla Omar <73911324+mollaomar991@users.noreply.github.com>
2024-07-09 11:29:59 +0600

    Create First_code.cpp

---
+++ Class 1`/First_code.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x, y;
+
+    printf("Enter two Data: ");
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    float average= (x+y)/2.0;
+    printf("%f",average);
+
+
+
+
+    return 0;
+}
